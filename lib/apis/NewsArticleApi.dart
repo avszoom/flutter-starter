eb6b1e70dccdcbe127234ea8b2048dd14f13b43b
Avinash Singh <avinashsingh@macbook-air.mynetworksettings.com>
2024-02-17 20:53:33 -0500

    query changed

--- lib/apis/NewsArticleApi.dart
+++ lib/apis/NewsArticleApi.dart
@@ -9,7 +9,9 @@ class NewsArticeApi {
   NewsArticeApi();
 
   Future<List<NewsArticleModel>> fetchNews() async {
-    final response = await http.get(Uri.parse('https://news-backend-ecru.vercel.app/news'));
+    final now = DateTime.now();
+    String fromDate = '${now.year}-${now.month.toString().padLeft(2, '0')}-${now.day.toString().padLeft(2, '0')}';
+    final response = await http.get(Uri.parse('https://news-backend-ecru.vercel.app/news?fromDate=$fromDate&toDate=$fromDate'));
     if (response.statusCode == 200) {
       List<dynamic> newsArticles = jsonDecode(response.body);
       return  newsArticles.map((article) => NewsArticleModel.fromJson(article)).toList();
